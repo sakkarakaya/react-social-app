027c015bc77de5512108330f4cf603067bdeb745
sakkarakaya <sakkarakaya@icloud.com>
2021-05-19 15:01:24 +0200

    added context

---
+++ src/context/AuthContext.js
@@ -0,0 +1,22 @@
+import React, { createContext, useEffect, useState } from 'react'
+import firebase from '../firebase/firebase.utils'
+
+export const FirebaseAuthContext = createContext()
+
+const AuthContext = (props) => {
+    const [currentUser, setCurrentUser] = useState()
+
+    useEffect(() => {
+        firebase.firebaseAuth.onAuthStateChanged((user)=>{
+            console.log("userr", user)
+            setCurrentUser(user)
+        })
+    }, [])
+    return (
+        <FirebaseAuthContext.Provider value={{currentUser}}>
+            {props.children}
+        </FirebaseAuthContext.Provider>
+    )
+}
+
+export default AuthContext
