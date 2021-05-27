92159dd0e863e98b532eb80e299a68f82f403fed
sakkarakaya <sakkarakaya@icloud.com>
2021-05-27 21:15:11 +0200

    added login turnary

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import Login from './pages/Login'
+import ForgotPassword from './pages/ForgotPassword'
 import Detail from './pages/Detail'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { FirebaseAuthContext } from './context/AuthContext'
 
 const AppRouter = () => {
+    const { currentUser } = useContext(FirebaseAuthContext);
     return (
         <Router>
             <Navbar />
             <Switch>
                 <Route path="/register" component={SignUp}/>
                 <Route path="/login" component={Login}/>
-                <Route path="/detail/:id" component={Detail}/>
+                <Route path="/detail/:id" component={currentUser ? Detail : Login}/>
+                <Route path="/forgot-password" component={ForgotPassword}/>
                 <Route path="/" component={Home}/>
             </Switch>
             <Footer />
